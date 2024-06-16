b338f69633d79a8f23505fcf1b8349e71be8e276
BingoX <142737559+BingoXDev@users.noreply.github.com>
2024-06-16 13:24:13 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+const app_id = 'c27acabad988e5f21529e57b8d9d3efb9f6ea695';
+const app_key = '96ee5e9e7d02f844cc4fd45e28d65f247313c201';
+// const baseUri = 'https://gl-sandbox.bingox.vip/mock';//模拟网关
+const baseUri = 'https://gl-sandbox.bingox.vip/openapi';//沙盒网关
+
+// const demo_email = 'helloworld@gmail.com';//示例邮箱
+const demo_email = '910496790@qq.com';//示例邮箱
+const demo_token = '589c72da7f297c73d37fe05f7b724095eddc5d10';//login接口返回的token
+
+module.exports = {
+    app_id,
+    app_key,
+    baseUri,
+    demo_email,
+    demo_token,
+}
