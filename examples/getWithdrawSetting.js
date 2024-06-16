a5d3d8c544eaa0671caefbcb3f21fba363d7dcf5
BingoX <BingoXcom@gmail.com>
2024-06-16 13:29:57 +0800

    1

---
+++ examples/getWithdrawSetting.js
@@ -0,0 +1,30 @@
+
+
+const axios = require("axios");
+const querystring = require('querystring');
+const md5 = require('md5');
+const config = require('../config.js');
+const utils = require('../utils.js');
+
+//原始参数
+const params = {
+  app_id: config.app_id,
+  token: config.demo_token,
+};
+
+//计算签名字符串
+const str = utils.getSignContent(params);
+const sign = md5(str + config.app_key);
+
+//转成表单数据
+const postData = querystring.stringify({ ...params, sign });
+// console.log(postData);
+//提交api调用
+axios.post(config.baseUri + '/Player/getWithdrawSetting', postData).then(res => {
+  if (res.data.status == 200) {//成功
+    console.log(res.data);
+  } else {
+    console.log('失败!' + res.data.mess);
+    console.log(res.data.data);
+  }
+});
